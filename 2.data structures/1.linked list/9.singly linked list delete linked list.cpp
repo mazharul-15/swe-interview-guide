fb20953b6106543c62c05132eb2ffb3c03a857ae
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-09-03 15:36:21 +0600

    update deletion of a whole singly linked list

--- 2.data structures/1.linked list/9.singly linked list delete linked list.cpp
+++ 2.data structures/1.linked list/9.singly linked list delete linked list.cpp
@@ -31,46 +31,9 @@ class linked_list_function {
 
     public:
 
-        //struct node *create_node(int , Node *, Node **);
-        struct node *create_node(int data, Node *tail, Node **head_ref) {
-
-            Node *new_node = new Node();
-
-            if(new_node == NULL) {
-                printf("OVERFLOW!!\n");
-                exit(1);
-            }
-
-            new_node->data = data;
-            new_node->next = NULL;
-
-            if(*head_ref == NULL) *head_ref = new_node;
-            else tail->next = new_node;
-
-            return new_node;
-        }
-        ///void display_list(Node *);
-        void display_list(Node *cur_node) {
-            if(cur_node == NULL) cout<< "Linked list is empty!!!\n";
-            while(cur_node != NULL) {
-
-                printf("%d ", cur_node->data);
-                cur_node = cur_node->next;
-            }
-            cout<< endl;
-        }
-        ///struct node *delete_list(Node *);
-        struct node *delete_list(Node *cur_node) {
-
-            Node *next_node = new Node();
-
-            while(cur_node != NULL) {
-                next_node = cur_node->next;
-                delete cur_node;
-                cur_node = next_node;
-            }
-            return cur_node;
-        }
+        Node* create_node(int , Node* , Node** );
+        void display_list(Node* );
+        Node* delete_list(Node* );
 };
 
 int main() {
@@ -91,9 +54,9 @@ int main() {
     return 0;
 }
 
-/*
+
 /// function definition
-List :: struct node *create_node(int data, Node *tail, Node **head_ref) {
+Node* List::create_node(int data, Node *tail, Node **head_ref) {
 
     Node *new_node = new Node();
 
@@ -105,30 +68,32 @@ List :: struct node *create_node(int data, Node *tail, Node **head_ref) {
     new_node->data = data;
     new_node->next = NULL;
 
-    if(*head == NULL) *head = new_node;
+    if(*head_ref == NULL) *head_ref = new_node;
     else tail->next = new_node;
 
     return new_node;
 }
 
-List::void display_list(Node *cur_node) {
+void List:: display_list(Node *cur_node) {
 
+    if(cur_node == NULL) printf("The Linked List is empty!!!!");
     while(cur_node != NULL) {
 
         printf("%d ", cur_node->data);
         cur_node = cur_node->next;
     }
+    printf("\n");
 }
 
-List::struct node *delete_list(Node *cur_node) {
+Node* List::delete_list(Node *cur_node) {
 
-    Node next_node = new Node();
+    Node *next_node = new Node();
 
     while(cur_node != NULL) {
         next_node = cur_node->next;
         delete cur_node;
         cur_node = next_node;
     }
+    return cur_node;
 }
-*/
 
