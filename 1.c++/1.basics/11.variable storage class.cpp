c7b093631fb6319f0413050280517355c6edd820
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-09-03 20:21:41 +0600

    modified into c++ folder

--- 1.c++/1.basics/11.variable storage class.cpp
+++ 1.c++/1.basics/11.variable storage class.cpp
@@ -5,15 +5,34 @@
 #include<utility>
 
 using namespace std;
+typedef class test Test;
+
+/// Mutable
+class test {
+    public:
+        mutable int y;
+        int x;
+
+        /// constructor
+        test() {
+            x = 10;
+            y = 33;
+        }
+};
 
 /// Global declaration
 int extern_var = 1000;
 
 void auto_storage_class(void);
+
 void extern_storage_class(void);
+
 void static_storage_class(void);
+
 void register_storage_class(void);
+
 void mutable_storage_class(void);
+
 void thread_local_storage_class(void);
 
 /// File inclusion
@@ -26,6 +45,21 @@ int main() {
     cout<< global_var << endl;
 
     extern_storage_class_2();
+
+    static_storage_class();
+    static_storage_class();
+    static_storage_class();
+
+    register_storage_class();
+
+    /// Mutable Section
+    const Test t1;
+    t1.y = 1000;
+    //t1.x = 2000;
+
+    cout<< "value of Y: " << t1.y << endl;
+    cout<< "value of X: " << t1.x << endl;
+
     return 0;
 }
 
@@ -39,6 +73,20 @@ void auto_storage_class(void) {
 
     cout<< a << " " << b << " " << c << " " << d << endl;
 
+}
+
+
+void static_storage_class(void) {
+    static int cn = 0;
+    cn++;
+    cout<< "Static Variable: " << cn << endl;
+}
+
+void register_storage_class(void) {
+    register int cn = 0;
+    int *ptr = &cn;
+
+    cout<< *ptr << " " << (*ptr = 100) << endl;
 }
 
 
