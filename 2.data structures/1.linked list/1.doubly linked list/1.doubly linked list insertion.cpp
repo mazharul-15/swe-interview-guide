b5ad2e49ad3289ee3e5b7c2bf70df25c883924ca
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-09-04 17:08:24 +0600

    insertion a node befoe given a node into doubly linked list

--- 2.data structures/1.linked list/1.doubly linked list/1.doubly linked list insertion.cpp
+++ 2.data structures/1.linked list/1.doubly linked list/1.doubly linked list insertion.cpp
@@ -37,6 +37,7 @@ class linked_list {
         void reverse_display(Node*);
         Node* insertion_middle(Node*);
         void insertion_after_node(int, int, Node*, Node**);
+        void insertion_before_node(int, int, Node*, Node**);
 };
 
 
@@ -67,6 +68,15 @@ int main() {
         printf("Main List after adding node: ");
         L.display_list(head);
         printf("Main List in reverse order after adding node: ");
+        L.reverse_display(tail);
+
+        printf("Enter two node 1st is node and 2nd new node value: ");
+        scanf("%d %d", &node_val, &val);
+        L.insertion_before_node(node_val, val, head, &head);
+
+        printf("Main List before adding node: ");
+        L.display_list(head);
+        printf("Main List in reverse order before adding node: ");
         L.reverse_display(tail);
 
     return 0;
@@ -118,6 +128,39 @@ void List::insertion_after_node(int val, int data, Node* cur_node, Node** tail_r
                 new_node->next = cur_node->next;
                 new_node->prev = cur_node;
                 cur_node->next = new_node;
+            }
+            break;
+        }
+        cur_node = cur_node->next;
+    }
+}
+
+/// insertion before a given node
+void List::insertion_before_node(int val, int data, Node* cur_node, Node** head_ref){
+     while(cur_node != NULL) {
+
+        if(cur_node->data == val) {
+
+            Node *new_node = new Node();
+            if(new_node == NULL) {
+                printf("OVERFLOW!!!\n");
+                exit(1);
+            }
+
+            new_node->data = data;
+            /// checking if head node
+            if(cur_node->prev == NULL) {
+                cur_node->prev = new_node;
+                new_node->next = cur_node;
+                new_node->prev = NULL;
+
+                *head_ref = new_node;
+            }
+            else {
+                cur_node->prev->next = new_node;
+                new_node->prev = cur_node->prev;
+                new_node->next = cur_node;
+                cur_node->prev = new_node;
             }
             break;
         }
