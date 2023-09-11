9344c37136d074996f8e2a10147be90d19c56d2c
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-09-11 21:08:47 +0600

    type conversion in c++

---
+++ 1.c++/1.basics/15.type conversion.cpp
@@ -0,0 +1,32 @@
+/*
+
+    Author:  Mazharul Islam
+             CSE, HSTU.
+
+    Program: This program is about "type conversion"
+
+    There are two type conversion:
+        1. implicit (automatic): variable upgraded to higher type
+        2. explicit:
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+    /// implicit type conversion
+    int x = 10;
+    char y = 'A';
+    long long int z = 1000;
+
+    cout<< (x+y+z)<< endl;
+
+    /// explicit type conversion
+    int m = 55;
+    printf("%0.5f", (float)m);
+    return 0;
+}
