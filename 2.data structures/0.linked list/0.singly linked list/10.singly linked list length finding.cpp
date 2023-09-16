f05f675bb5ee02214d4834c7f1e1cdbe65e95d66
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-09-16 22:39:54 +0600

    finding the linked list length

---
+++ 2.data structures/0.linked list/0.singly linked list/10.singly linked list length finding.cpp
@@ -0,0 +1,110 @@
+/*
+    This program will find the length of a linked list
+    using recursive method
+
+    Time Complexity: O(n);
+    Space Complexity: O(n);
+
+)*/
+#include<iostream>
+#include<limits.h>
+#include<algorithm>
+#include<utility>   /// pair
+#include<vector>
+#include<list>      /// doubly linked list
+#include<forward_list> /// singly linked list
+#include<deque>     /// double ended queue
+#include<stack>
+#include<queue>     /// queue + priority_queue
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+
+using namespace std;
+
+typedef class node Node;
+
+class node {
+    public:
+        int data;
+        Node *next;
+};
+
+Node *create_node(int , Node *, Node**);
+bool searching_node(int , Node *);
+int length(Node*);
+void display_list(Node *);
+
+int main() {
+
+    Node *cur_node, *head = NULL;
+
+    int n, val;
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        scanf("%d", &val);
+        cur_node = create_node(val, cur_node, &head);
+    }
+
+    scanf("%d", &val);
+
+    bool status = searching_node(val, head);
+    cout<< "Value: "<< status << endl;
+    if(status) printf("YES\n");
+    else printf("NO\n");
+    ///display_list(head);
+
+    cout<< "The length : " << length(head) << endl;
+
+    return 0;
+}
+
+/// creating node
+Node *create_node(int data, Node *cur_node, Node** head_ref) {
+    Node *new_node = new Node();
+
+    if(new_node == NULL) {
+        printf("OVERFLOW!!!\n");
+        exit(1);
+    }
+
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if(*head_ref == NULL) *head_ref = new_node;
+    else cur_node->next = new_node;
+
+    return new_node;
+}
+
+/// searching a node using recursion
+bool searching_node(int data, Node *cur_node) {
+
+    if(cur_node == NULL) return false;
+
+    if(cur_node->data == data) return true;
+    else return searching_node(data, cur_node->next);
+
+}
+
+/// finding the length
+int length(Node *cur) {
+
+    if(cur == NULL) return 0;
+
+    return length(cur->next) + 1;
+}
+
+/// printing the linked list
+void display_list(Node *cur_node) {
+    while(cur_node != NULL) {
+        printf("%d ", cur_node->data);
+        cur_node = cur_node->next;
+    }
+    printf("\n");
+}
+
+
+
