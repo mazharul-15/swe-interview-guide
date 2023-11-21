596f054e0a0476da7e8d410e54411393b812d46e
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-11-21 10:45:12 +0600

    operator overloading

---
+++ 1.c++/5.oop/3.ploymorphism/0.function overloading/1.can't be overloaded.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int a, int b) {
+
+    return (a+b);
+}
+
+char f(int a, int b) {
+
+    return 'A';
+}
+
+int main() {
+
+    int func(int a, int b, int c);
+
+    cout<< func(1, 2, 3) << endl;
+
+    cout<< f(10, 10) << endl;
+
+    cout<< f(34, 34) << endl;
+
+    return 0;
+}
+
+int func(int a, int b, int c) {
+
+        return (a+b+c);
+}
+
---
+++ 1.c++/5.oop/3.ploymorphism/1.operator overloading/0.intro.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class operatorOverloading {
+
+    private:
+        int a;
+        int b;
+
+    public:
+
+        operatorOverloading() {}
+
+        operatorOverloading(int u, int v) {
+            a = u;
+            b = v;
+        }
+
+        // this is automatically called when two object is use
+        // obj1 + obj2
+        operatorOverloading operator+(operatorOverloading &obj) {
+
+            operatorOverloading res;
+
+            res.a = a + obj.a;
+            res.b = b + obj.b;
+
+            return res;
+        }
+
+        void display() {
+            cout<< a << " " << b << "\n";
+        }
+
+};
+
+int main() {
+
+    operatorOverloading obj1(4, 6), obj2(6, 4);
+
+    operatorOverloading obj3 = obj1 + obj2;
+    obj3.display();
+
+    return 0;
+}
