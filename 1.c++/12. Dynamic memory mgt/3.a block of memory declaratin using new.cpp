d20debb35c9fd899b3de4f6b30ee3d8828bdce8e
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-11-28 09:32:17 +0600

    array declarating using new operator

---
+++ 1.c++/12. Dynamic memory mgt/3.a block of memory declaratin using new.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int *arr = new int[100];
+
+    for(int i = 0; i < 10; i++)
+    {
+        arr[i] = 5 + i;
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        cout<< arr[i] << " ";
+    }
+    return 0;
+}
