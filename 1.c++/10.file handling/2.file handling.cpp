cca6d96050f0d141b0a255271ff1faa4a31708e6
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-11-30 09:07:39 +0600

    file i/o

---
+++ 1.c++/10.file handling/2.file handling.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    FILE *f1, *f2;
+
+    f1 = freopen("input.txt", "r", stdin);
+    f2 = freopen("outputSUM.txt", "w", stdout);
+
+    int a, b;
+
+    while(!feof(f1))
+    {
+        cin>> a >> b;
+
+        int sum = a + b;
+        cout<< sum << endl;
+
+    }
+
+    fclose(f1);
+    fclose(f2);
+    return 0;
+}
