484ba7da0058a7425f7dec612002b5c8eb37634c
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-02-22 23:18:48 +0600

    added do-while loop

---
+++ 1.c++/3.loop/5.do-while loop.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin>> t;
+    do
+    {
+        cin>> n;
+        cout<< n << endl;
+    }while(t--);
+
+    return 0;
+}
