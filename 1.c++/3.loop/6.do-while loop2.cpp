498ddec6315c8d7daae8294099ead421271567d7
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-02-26 00:33:46 +0600

    adding do-while loop2

---
+++ 1.c++/3.loop/6.do-while loop2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void myFunc(int t);
+
+int main() {
+
+    int test;
+    cin>> test;
+    myFunc(test);
+    return 0;
+}
+
+void myFunc(int T) {
+    do {
+        cout<< "hello" << endl;
+    } while(T--);
+}
